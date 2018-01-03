b998e89026d871448a35893bafe9684cab982d37
rhinnawi <rhinnawi95@gmail.com>
2018-01-02 22:03:48 -0500

    Homepage with new organisational comments

--- OW_RH_Homepage/src/Homepage.java
+++ OW_RH_Homepage/src/Homepage.java
@@ -2,8 +2,21 @@
 public class Homepage {
 
     public static void main(String[] args) {
-        //HI OLIVIA
-        //HI RANI :D
+        //Ask user for output file name
+    	
+    	//Create file
+    	
+    	//Output title and header to file. Ask user for site name
+    	
+    	//Upload background image (user inputed?)
+    	
+    	//Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
+    	
+    	//Body
+    	
+    	//Footer
+    	
+    	System.out.println("Olivia is dope.");
 
     }
 
