2bf811d3c6ee2f422e1bb6fcf5de0fb5cf9f1821
olivi <olivi@192.168.200.67>
2018-01-03 18:36:41 -0500

    Began home page styling with css file

--- OW_RH_Homepage/src/Homepage.java
+++ OW_RH_Homepage/src/Homepage.java
@@ -16,12 +16,33 @@ public class Homepage extends htmltemplate {
         //String fileName = in.nextLine();
 
         //Output title and header to file. Ask user for site name
-        htmltemplate.outputHeader("test", file);
+        htmltemplate.outputHeader("Home", file);
+
+        file.println("<body>");
+        file.println("<h1> OliviaRani </h1>");
+        file.println("<hr>");
+        file.println("<h2> Welcome to our page! </h2>");
+
+        SimpleWriter page2 = new SimpleWriter1L("WebContent/link.html");
+        file.print("Click on this ");
+        file.print("<a href=\"link.html\">link </a>");
+        file.println("for a surprise :)");
+
+        //code for page 2
+
+        htmltemplate.outputHeader("Surprise!", page2);
+
+        page2.println("<img src=\"funny pic.jpg\"><img>");
+        htmltemplate.outputFooter(page2);
 
         //Upload background image (user inputed?)
 
         //Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
 
+        //add css elements
+
+        file.println(
+                "<link href=\"homepageStyle.css\" rel=\"stylesheet\" type=\"text/css\"></link>");
         //Body
         file.println("<body><p>Olivia is dope</p>");
 
