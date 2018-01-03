f457e2755d0272bc7e94dd5d6d8939d5551e09c1
rhinnawi <rhinnawi95@gmail.com>
2018-01-02 23:11:42 -0500

    Temporarily outputing to index.html

--- OW_RH_Homepage/src/Homepage.java
+++ OW_RH_Homepage/src/Homepage.java
@@ -1,27 +1,33 @@
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
 
 public class Homepage extends htmltemplate {
 
-    
-    public static void outputHeader() {}
-    
-    
     public static void main(String[] args) {
         //Ask user for output file name
-    	
-    	//Create file
-    	
-    	//Output title and header to file. Ask user for site name
-    	
-    	//Upload background image (user inputed?)
-    	
-    	//Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
-    	
-    	//Body
-    	
-    	//Footer
-    	
-    	System.out.println("Olivia is dope.");
-    	System.out.println("I can't think of anything clever to add :/");
+        SimpleWriter out = new SimpleWriter1L(),
+                file = new SimpleWriter1L("index.html");
+        SimpleReader in = new SimpleReader1L();
+        //out.println("File name: ");
+
+        //Create file
+        //String fileName = in.nextLine();
+
+        //Output title and header to file. Ask user for site name
+
+        //Upload background image (user inputed?)
+
+        //Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
+
+        //Body
+
+        //Footer
+
+        //Close file
+
+        System.out.println("I can't think of anything clever to add :/");
     }
 
 }
