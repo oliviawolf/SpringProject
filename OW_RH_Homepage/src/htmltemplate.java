c69ef214c8702d68f92ada55ad9c09a74f00ba09
olivi <olivi@192.168.200.67>
2018-01-02 22:32:55 -0500

    add osu components and output header and footer method to template

--- OW_RH_Homepage/src/htmltemplate.java
+++ OW_RH_Homepage/src/htmltemplate.java
@@ -1,13 +1,26 @@
+import components.simplewriter.SimpleWriter;
 
 public class htmltemplate {
-//template to create different components of a well formatted html file
-    
+    //template to create different components of a well formatted html file
+
     //method to create headers
-    
+    public static void outputHeader(String title, SimpleWriter out) {
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>" + title + "</title>");
+        out.println("</head>");
+
+    }
+
     //method do create footers
-    
+    public static void outputFooter(SimpleWriter out) {
+        out.println("</body>");
+        out.println("</html>");
+    }
+
     //different body methods
     public static void main(String[] args) {
-        
+
     }
 }
