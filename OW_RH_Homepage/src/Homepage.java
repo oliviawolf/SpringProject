17263f09ee012438add9d493df140b6e3433b06b
olivi <olivi@192.168.200.67>
2018-01-02 22:09:45 -0500

    added A CONMENT

--- OW_RH_Homepage/src/Homepage.java
+++ OW_RH_Homepage/src/Homepage.java
@@ -3,21 +3,22 @@ public class Homepage {
 
     public static void main(String[] args) {
         //Ask user for output file name
-    	
-    	//Create file
-    	
-    	//Output title and header to file. Ask user for site name
-    	
-    	//Upload background image (user inputed?)
-    	
-    	//Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
-    	
-    	//Body
-    	
-    	//Footer
-    	
-    	System.out.println("Olivia is dope.");
 
+        //Create file
+
+        //Output title and header to file. Ask user for site name
+
+        //Upload background image (user inputed?)
+
+        //Create tabs, maybe embedded within a colored/designed band. Link other HTML pages to tabs. Later: prompt user for design preferences
+
+        //Body
+
+        //Footer
+
+        System.out.println("Olivia is dope.");
+
+        //OLIVIa is here
     }
 
 }
