b4f6e75685b6dcf8dd0934ace71136ea7cbe466e
olivi <olivi@192.168.200.67>
2018-01-02 22:00:19 -0500

    Added template class and created html files in WebContent folder

---
+++ OW_RH_Homepage/src/htmltemplate.java
@@ -0,0 +1,13 @@
+
+public class htmltemplate {
+//template to create different components of a well formatted html file
+    
+    //method to create headers
+    
+    //method do create footers
+    
+    //different body methods
+    public static void main(String[] args) {
+        
+    }
+}
