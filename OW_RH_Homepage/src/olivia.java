d55bae28abd2fd969acb46f3ba34f12c9698fd78
olivi <olivi@192.168.200.67>
2018-01-04 23:44:25 -0500

    added an audio element to olivia page and added play and pause buttons

--- OW_RH_Homepage/src/olivia.java
+++ OW_RH_Homepage/src/olivia.java
@@ -18,6 +18,9 @@ public class olivia extends htmltemplate {
 
         createTabs(tOlivia, olivia);
 
+        //add a song to page
+        olivia.println(
+                "<embed id=\"sound\" src=\"C:\\Users\\olivi\\Music\\A music\\01 - Like A Star.mp3\" autostart=\"false\" width=\"0\" height=\"0\">");
         //create button
 
         createButton("click me", "myFunction()", olivia);
@@ -26,14 +29,20 @@ public class olivia extends htmltemplate {
 
         createButton("click to add your own image", "addImage()", olivia);
 
+        createButton("play a song", "playSong()", olivia);
+
+        createButton("pause", "pauseSong()", olivia);
+
         olivia.println(
                 "<img id=\"baby\" src=\"funny pic.jpg\" style=\"visibility:hidden\" width=\"400\" height=\"height\" />");
         //include javascript
 
-        olivia.println("<script src=\"olivia.js\"></script>");
-
+        //olivia.println(
+        //      "<embed id=\"sound\" src=\"null\" autostart=\"false\" width=\"0\" height=\"0\">");
         olivia.println(
                 "<img id=\"new\" src=\"null\" style=\"visibility:hidden\" width=\"400\" height=\"height\" />");
+
+        olivia.println("<script src=\"olivia.js\"></script>");
         // olivia.println("<img src=\"funny pic.jpg\"><img>");
         htmltemplate.outputFooter(olivia);
 
